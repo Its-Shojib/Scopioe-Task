df7d2d92594b16370832acc211526fda9cdf172e
Md Shojib Hossain <mdshojib922@gmail.com>
2024-08-02 18:11:08 +0600

    Register Page Layout Updated

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,19 @@
-
+import img from '../../assets/Login-Register.png'
 
 const Register = () => {
     return (
-        <div>
-            <p className="font-nanum font-bold">Hi! I am Register </p>
+        <div className='p-10'>
+            <div className="flex justify-center items-center gap-20">
+                <div className='flex-1'>
+                    <h1 className='font-nanum text-2xl font-extrabold text-blue-600'>LOGO</h1>
+                    <h3>Sign In To Your Account </h3>
+
+                    <p>{`Welcome Back! By click the sign up button, you're agree to Zenitood Terms and Service and acknlowledge the Privacy and Policy`}</p>
+                </div>
+                <div className='flex-1'>
+                    <img src={img} className='w-10/12 h-5/6' alt="" />
+                </div>
+            </div>
         </div>
     );
 };
