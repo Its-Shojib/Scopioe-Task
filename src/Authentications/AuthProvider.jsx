2929e4edd3865e170b6285f0214f7fcc6edcd0fb
Md Shojib Hossain <mdshojib922@gmail.com>
2024-08-03 19:50:38 +0600

    Firebase Installed and Setup

---
+++ src/Authentications/AuthProvider.jsx
@@ -0,0 +1,82 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, updateProfile } from 'firebase/auth'
+import auth from "../Firebase/Firebase.config";
+
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    let [user, setUser] = useState(null);
+    let [loading, setLoading] = useState(true);
+    let googleProvider = new GoogleAuthProvider();
+
+    let createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    let SignInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    let Logout = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+    let googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+    let updateUserProfile = (name, image) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: image
+        });
+    }
+
+    useEffect(() => {
+        let unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('observing: ', currentUser?.displayName);
+            let userInfo = {
+                email: currentUser?.email
+            }
+            if (currentUser) {
+                localStorage.setItem('user', userInfo);
+                setLoading(false);
+
+            }
+            else {
+                localStorage.removeItem('user');
+                setLoading(false);
+            }
+
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+
+    let authInfo = {
+        user,
+        createUser,
+        SignInUser,
+        Logout,
+        googleSignIn,
+        loading,
+        setUser,
+        updateUserProfile,
+        setLoading
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+};
+AuthProvider.propTypes = {
+    children: PropTypes.node,
+}
+export default AuthProvider;
